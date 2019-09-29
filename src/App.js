e3c8badc623617b06ea6c86a2e3de3eed1e74c6d
Yoonjin <yoon7ks@gmail.com>
2019-09-29 12:21:53 +0900

    컴포넌트 반복 map 사용

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import React, { Component } from 'react';
-import ScrollBox from './ScrollBox';
+import React, { Component } from "react";
+import IterationSample from "./IterationSample";
 
 class App extends Component {
   // 컴포넌트 태크 사이에 내용을 보여주는 props가 children이다.
   render() {
     return (
-
-      <div>
-
-        <ScrollBox ref={(ref) => this.scrollBox = ref} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-              맨 밑으로
-        </button>
-
-      </div>
-
+      <ul>
+        <IterationSample />
+      </ul>
     );
   }
 }
--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const IterationSample = () => {
-  const names = ['눈사람', '얼음', '눈', '바람'];
-  const nameList = names.map(name => <li>{name}</li>);
+  const [names, setNames] = useState([
+    { id: 1, text: "눈사람1" },
+    { id: 2, text: "눈사람2" },
+    { id: 3, text: "눈사람3" },
+    { id: 4, text: "눈사람4" }
+  ]);
+
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5); //새로운 항목을 추가할 때 사용할 id
+
+  const onChange = e => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId, // nextId를 id로 설정
+      text: inputText
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText("");
+  };
+  const onRemove = id => {
+    const nextNames = names.filter(name => name.id !== id); // 해당 id를 없애고 싶을때 filter사용
+    setNames(nextNames);
+  };
+
+  const namesList = names.map(name => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
 
   return (
-      <ul>
-        {nameList};
-      </ul>
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{namesList}</ul>
+    </>
   );
 };
 
